a5d6c0aaad7df2cdac60ae9bae122a35a664b631
LuisAlbertoFeernandez <luisfernandezcardenas0@gmail.com>
2023-01-06 14:50:57 -0500

    Ejercicio 5  Numeros Finonaci

--- er.py
+++ er.py
@@ -1,10 +1,12 @@
 def fibonacci(n):
-  if n == 0:
-    return 0
-  elif n == 1:
-    return 1
+  if n <= 1:
+    return n
   else:
     return fibonacci(n-1) + fibonacci(n-2)
 
+# Prueba del código
 
+print(fibonacci(5))
 print(fibonacci(10))
+print(fibonacci(30))
+
