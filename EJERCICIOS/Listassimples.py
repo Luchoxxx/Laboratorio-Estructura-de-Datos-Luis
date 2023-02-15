9d07e462b343c83f526e40fcc6575c0b1d55b216
LuisAlbertoFeernandez <luisfernandezcardenas0@gmail.com>
2023-02-15 10:43:30 -0500

    "Listas simples"

--- EJERCICIOS/Listassimples.py
+++ EJERCICIOS/Listassimples.py
@@ -1,4 +1,46 @@
-
 print("Hola mundo listas simples")
 
 
+##Creamos una clase nodo
+
+class Nodo:
+    def __int__(self, nombre, cedula, sig):
+        self.nombre = nombre
+        self.cedula = cedula
+        self.sig = sig
+
+    def __str__(self):
+        return "%s %s" % (self.nombre, self.cedula)
+
+
+class listasimples:
+
+    def __int__(self):
+        self.cabeza = None
+        self.cola = None
+
+    # CREAMOS UNA FUNCION AGREGAR
+
+    def agrega(self, elemento):
+
+        if self.cabeza == None:
+            self.cabeza = elemento
+
+        if self.cola != None:
+            self.cola.sig = elemento
+
+        self.cola = elemento
+
+
+if __name__ == "__main__":
+    oLisSimpl = listasimples()
+    while True:
+        print("----Menu-----\n "
+              + "1. Agregar")
+        numero = int(input("Ingrese la opcion: "))
+
+        if numero == 1:
+            nombre = input("Ingrese el nombre: ")
+            Cedula = input("Ingrese la cedula: ")
+            oNodo = Nodo(nombre, Cedula)
+            oLisSimpl.agrega(oNodo)
